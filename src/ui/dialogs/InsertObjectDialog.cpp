db728adc14f7e93b0bbc314117ab8f265d16c52b
Jota Blen <jotablen@gmail.com>
2026-06-12 08:15:59 +0200

    InsertObjectDialog

--- src/ui/dialogs/InsertObjectDialog.cpp
+++ src/ui/dialogs/InsertObjectDialog.cpp
@@ -1,5 +1,9 @@
 #include "InsertObjectDialog.h"
+#include <QComboBox>
+#include <QLineEdit>
+#include <QCheckBox>
 #include <QFormLayout>
+#include <QVBoxLayout>
 #include <QDialogButtonBox>
 #include <QLabel>
 #include <core/ObjectFactory.h>
@@ -8,18 +12,87 @@ namespace ExpressDesigner {
 
 InsertObjectDialog::InsertObjectDialog(QWidget* parent) : QDialog(parent)
 {
-    setWindowTitle(QStringLiteral("Insert Object"));
+    setWindowTitle(QStringLiteral("Insert Object Dialog"));
+
     auto* layout = new QVBoxLayout(this);
+
+    // --- Type combo ---
     auto* form = new QFormLayout();
-    form->addRow(QStringLiteral("Type:"), new QLabel(QStringLiteral("Select object type")));
+    m_typeCombo = new QComboBox(this);
+    m_typeCombo->addItem(QStringLiteral("Point Object"));
+    m_typeCombo->addItem(QStringLiteral("Line Object"));
+    m_typeCombo->addItem(QStringLiteral("Arc Object"));
+    m_typeCombo->addItem(QStringLiteral("Curve Object"));
+    m_typeCombo->setCurrentIndex(0);
+    form->addRow(QStringLiteral("Type:"), m_typeCombo);
+
+    // --- Name edit ---
+    m_nameEdit = new QLineEdit(this);
+    m_nameEdit->setText(QStringLiteral("NewObject"));
+    form->addRow(QStringLiteral("Name:"), m_nameEdit);
+
     layout->addLayout(form);
+
+    // --- Is WF checkbox ---
+    m_isWfCheck = new QCheckBox(QStringLiteral("Object is a WF"), this);
+    m_isWfCheck->setChecked(true);
+    layout->addWidget(m_isWfCheck);
+
+    // --- Is Virtual WF checkbox ---
+    m_isVirtualWfCheck = new QCheckBox(QStringLiteral("Wavefront is virtual"), this);
+    m_isVirtualWfCheck->setChecked(false);
+    m_isVirtualWfCheck->setVisible(false); // hidden by default (matches original)
+    layout->addWidget(m_isVirtualWfCheck);
+
+    connect(m_isWfCheck, &QCheckBox::toggled, this, &InsertObjectDialog::onIsWFChanged);
+
+    // --- Buttons ---
     auto* buttons = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
     connect(buttons, &QDialogButtonBox::accepted, this, [this]() {
-        m_createdObject = ObjectFactory::createFromType(ObjectType::Point, QStringLiteral("NewObject"), false, this);
-        accept();
+        createObject();
+        if (m_createdObject)
+            accept();
     });
     connect(buttons, &QDialogButtonBox::rejected, this, &QDialog::reject);
     layout->addWidget(buttons);
+}
+
+void InsertObjectDialog::onIsWFChanged(bool checked)
+{
+    // Enable the virtual WF checkbox only when "Is WF" is checked.
+    // When disabled, also uncheck it (matches original behavior).
+    m_isVirtualWfCheck->setEnabled(checked);
+    if (!checked)
+        m_isVirtualWfCheck->setChecked(false);
+}
+
+void InsertObjectDialog::createObject()
+{
+    // Map combo index to ObjectType base
+    ObjectType baseType;
+    switch (m_typeCombo->currentIndex()) {
+    case 0: baseType = ObjectType::Point; break;
+    case 1: baseType = ObjectType::Line;  break;
+    case 2: baseType = ObjectType::Arc;   break;
+    case 3:
+    default: baseType = ObjectType::Curve; break;
+    }
+
+    // Apply WF / VirtualWF masks
+    ObjectType finalType = baseType;
+    if (m_isWfCheck->isChecked()) {
+        if (m_isVirtualWfCheck->isChecked())
+            finalType = withVirtualWF(baseType);
+        else
+            finalType = withWavefront(baseType);
+    }
+
+    QString name = m_nameEdit->text().trimmed();
+    if (name.isEmpty())
+        name = QStringLiteral("NewObject");
+
+    bool isWF = m_isWfCheck->isChecked();
+    m_createdObject = ObjectFactory::createFromType(finalType, name, isWF, this);
 }
 
 } // namespace ExpressDesigner
--- src/ui/dialogs/InsertObjectDialog.h
+++ src/ui/dialogs/InsertObjectDialog.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <QDialog>
 #include <core/CustomObject.h>
+#include <core/ObjectTypes.h>
+
+class QComboBox;
+class QLineEdit;
+class QCheckBox;
 
 namespace ExpressDesigner {
 
@@ -11,7 +16,16 @@ public:
     ~InsertObjectDialog() override = default;
     CustomObject* createdObject() const { return m_createdObject; }
 
+private slots:
+    void onIsWFChanged(bool checked);
+
 private:
+    void createObject();
+
+    QComboBox* m_typeCombo = nullptr;
+    QLineEdit* m_nameEdit = nullptr;
+    QCheckBox* m_isWfCheck = nullptr;
+    QCheckBox* m_isVirtualWfCheck = nullptr;
     CustomObject* m_createdObject = nullptr;
 };
 
